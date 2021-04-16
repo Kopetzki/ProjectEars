3bfdee8ade4dd2b0b8e7c8f4fd3db76531f4f846
Kopetzki <esabanovic@ufl.edu>
2021-04-16 10:55:34 -0400

    ignore node_MODUles466

--- backend/urls.py
+++ backend/urls.py
@@ -14,17 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 
 from django.conf import settings
-from django.views.generic import TemplateView
 from django.conf.urls.static import static #imported to work with static files
 from .views import index
 from rest_framework import routers
 
 
 urlpatterns = [
-    re_path('.*', TemplateView.as_view(template_name='index.html')),
+    path('', index, name='index'),
     path('admin/', admin.site.urls),
     path('api/', include('books.urls')),
    
