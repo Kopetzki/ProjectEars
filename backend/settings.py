780e28651488d0b1f0362805bdc950c84543db9d
Kopetzki <esabanovic@ufl.edu>
2021-04-16 20:27:36 -0400

    ignore node_MODUles422105

--- backend/settings.py
+++ backend/settings.py
@@ -147,9 +147,9 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'build', 'media')
 
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ORIGIN_WHITELIST = [
     'https://localhost:3000',
